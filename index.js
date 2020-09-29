21807b2af9fded3a87a18fa5d4831f59a9000bac
Корниенко Алексей Александрович <Kornienko-A@gis.lan>
2020-09-29 23:21:34 +0300

    feat: make task

---
+++ index.js
@@ -0,0 +1,6 @@
+const towelSort = require('./src/index.js');
+const matrix = [
+    [1, 2],
+    [3, 4],
+  ];
+  console.log(towelSort(matrix));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
 // You should implement your task here.
+const sorter = (a, b) => {
+  if (a > b) {
+    return -1;
+  } else if (a < b) {
+    return 1;
+  }
+  return 0;
+};
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+    return [];
+  }
+  const fn = (acc, arr, deepCount) => {
+    if (arr.length === 0) {
+      return acc;
+    }
+
+    const [item, ...rest] = arr;
+
+    if (Array.isArray(item)) {
+      const newAcc = deepCount % 2 === 0 ? [...acc, ...fn([], item, 1).sort(sorter)] : [...acc, ...fn([], item, 1)];
+      return fn(newAcc, rest, deepCount + 1);
+    }
+    return fn([...acc, item], rest, deepCount + 1);
+  };
+
+  return fn([], matrix, 1);
 }
